8e3cf5236876ea041018aab14c77109eb8bb4fad
Andrés Zapata <andresmauricio030400@gmail.com>
2023-02-17 13:08:12 -0500

    Inicialización de los ejercicios

---
+++ 01/script.js
@@ -0,0 +1,3 @@
+console.log("Hola, mundo");
+console.log("Soy el primer script");
+alert("Recuerda tener activado el soporte de JavaScript en tu navegador"); // Mensaje de alerta.
---
+++ 02/script.js
@@ -0,0 +1,3 @@
+let mensaje = `Hola Mundo!
+Qué fácil es incluir 'comillas simples' y "comillas dobles"`;
+alert(mensaje);
